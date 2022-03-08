650dfe6dfb3b32262f680dabf72152114b6a9a08
Nova Wtrk <nova.wtrk@pm.me>
2022-03-08 16:15:51 +0000

    webscraping complete, added prettier

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config/config.json');
+const { scrape } = require('./modules/scrape');
 
 // Create the bot client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // Run this code once when the client is ready
 client.once('ready', () => {
-	console.log(`${client.user.tag} is ready!`);
+  console.log(`${client.user.tag} is ready!`);
+
+  // Scrape the bus numbers and locations from the website
+  scrape().then(console.log).catch(console.error);
 });
 
 client.login(token);
